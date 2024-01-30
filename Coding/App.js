c7018db52fce5033dd9f11974e006b05e2891532
Uday Chavan <udaymchavan228@gmail.com>
2024-01-30 15:35:56 +0530

    created an initial card, still in process

--- Coding/App.js
+++ Coding/App.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 const Header = () => {
   return(
-  <div className="Header">
+  <div className="header">
     <div className="logo-container">
       <img
         className="logo"
@@ -25,10 +25,33 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return(
+    <div className="res-card">
+      <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/405645b3118d83e89db4c65361e43733" alt="" />
+      <h3>Uday Foods</h3>
+      <h4>Biryani, North India, Asian</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return(
+    <div className="bod">
+      <div className="search">Search</div>
+      <div className="res-container">
+        
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
+
 const AppLayout = () =>{
   return (
     <div className="app">
       <Header/>
+      <Body />
     </div>
   )
 }
